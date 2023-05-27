da52a01113bb063f6108325b329b3fc2ab131149
maxefimov <efimov.top.academy@gmail.com>
2023-05-27 17:14:40 +0300

    Добавьте файлы проекта.

---
+++ NetworkSocketsApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Sockets;
+
+using (Socket socketTcp = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+{
+    try
+    {
+        socketTcp.Shutdown(SocketShutdown.Both);
+    }
+    catch(Exception e)
+    {
+        Console.WriteLine(e.Message);
+    }
+    finally { socketTcp.Close(); }
+}
+
+Socket socketUdp = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
